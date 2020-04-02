34ca3bd4a868bbbb27b9e7b7de60e56d9ab315fb
Gwlanbzh <62286593+Gwlanbzh@users.noreply.github.com>
2020-04-02 19:06:24 +0200

    Add a confirmation echo.

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo gcc ./no.c -o /usr/bin/no && sudo cp ./no.1.gz /usr/share/man/man1/no.1.gz
+sudo gcc ./no.c -o /usr/bin/no && sudo cp ./no.1.gz /usr/share/man/man1/no.1.gz && echo "Installation done."
